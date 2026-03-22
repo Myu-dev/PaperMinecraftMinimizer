5f7903d89da316d33d35378ca5d1829286100bbe
Myu <chitoseame.jp@gmail.com>
2026-03-23 08:53:09 +0900

    stageの取得の修正

--- ScratchJSON.js
+++ ScratchJSON.js
@@ -8,15 +8,19 @@ class ScratchJson{
         else if(typeof value==="string")this.data=JSON.parse(value);
     }
 
+    get stage(){
+        return this.data.targets.find(v=>v.isStage)
+    }
+
     //使わないので未完成
     get globalVariables(){
-        const stage=this.data.target[0];
+        const stage=this.stage;
         const variables=stage.variables;
         this.data
     }
 
     get globalLists(){
-        const stage=this.data.target[0];
+        const stage=this.stage;
         const Lists=Object.values(stage.lists);
         return Lists.map(v=>{return {name:v[0],value:v[1]}});
     }
